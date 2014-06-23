5c20239d00799c577b31776ac3d884263a59d89d
TrevorFoster <trevor.foster@hotmail.ca>
2014-06-23 01:17:41 -0400

    Small refactor of javascript. Href on image

--- js/pageHandler.js
+++ js/pageHandler.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
-    var $root = $("html, body");
+    // Cached variables
     var $locations = ["projects", "portfolios", "links"];
     var $homepage = "projects";
+
     var loaded = "";
     var selectedIndex = -1;
 
-    var loadContent = function(page) {
-        if (page.hasOwnProperty("animate")) {
-            $("#content").load(page.name + ".html", function() {
+    function loadContent(loadArgs) {
+        if (loadArgs.hasOwnProperty("animate")) {
+            $("#content").load(loadArgs.name + ".html", function() {
                 $(".animate").each(function() {
-                    $(this).css("right", page.animate.offset);
+                    $(this).css("right", loadArgs.animate.offset);
                     $(this).animate({
-                        right: page.animate.operator
+                        right: loadArgs.animate.operator
                     }, 500);
                 });
             });
         } else {
-            $("#content").load(page.name + ".html");
+            $("#content").load(loadArgs.name + ".html");
         }
 
-        document.location.hash = page.name;
-        loaded = page.name;
+        document.location.hash = loadArgs.name;
+        loaded = loadArgs.name;
 
         // a nav element has been clicked
-        if (page.changeByName) {
-            var a = $("ul#nav li a[href$=" + page.name + "]");
+        if (loadArgs.changeByName) {
+            var a = $("ul#nav li a[href=" + loadArgs.name + "]");
             if (selectedIndex != -1) {
                 $("ul#nav li:eq(" + selectedIndex.toString() + ")").toggleClass("active");
             }
@@ -34,8 +35,24 @@ $(document).ready(function() {
         }
     }
 
+    function changeSelected(newIndex) {
+        if (selectedIndex == newIndex) {
+            return;
+        }
+
+        if (selectedIndex != -1) {
+            $("ul#nav li:eq(" + selectedIndex.toString() + ")").toggleClass("active");
+        }
+        $("ul#nav li:eq(" + newIndex.toString() + ")").toggleClass("active");
+
+        selectedIndex = newIndex;
+    }
+
+    // When the hash is changed navigate to that place if it exists
     window.onhashchange = function() {
         var hash = document.location.hash;
+
+        // Remove the hash from the hash
         hash = hash.replace(/\#/g, "");
 
         if (hash === loaded) {
@@ -48,37 +65,37 @@ $(document).ready(function() {
                 name: hash,
                 changeByName: true
             });
-            // not valid url go back to previous page
         } else {
+            // not valid url go back to previous page
             window.history.back();
         }
 
         return false;
     }
 
-    // if no hash assigned redirect to projects page for now
+    // if no hash assigned redirect to homepage
     if (!document.location.hash) {
         loadContent({
             name: $homepage,
             changeByName: true
         });
-        // there is a hash left
     } else {
         var hash = document.location.hash;
+        var toLoad;
+
         hash = hash.replace(/\#/g, "");
 
         // check hash validity
         if ($locations.indexOf(hash) > -1) {
-            loadContent({
-                name: hash,
-                changeByName: true
-            });
+            toLoad = hash;
         } else {
-            loadContent({
-                name: $homepage,
-                changeByName: true
-            });
+            toLoad = $homepage;
         }
+
+        loadContent({
+            name: toLoad,
+            changeByName: true
+        });
     }
 
     // pulls down top nav bar tabs
@@ -89,36 +106,42 @@ $(document).ready(function() {
     // handle menu clicks
     $("ul#nav li a").click(function() {
         var self = $(this);
+        var newIndex = $("ul#nav li a").index(self);
 
-        if (!(self.parent().hasClass("active"))) {
-            var newIndex = $("ul#nav li a").index(self);
-            var offset = (newIndex > selectedIndex) ? 2000 : -2000;
-            var operator = "+=" + offset.toString();
-
-            changeSelected(newIndex);
-
-            $(".animate").each(function() {
-                $(this).animate({
-                    right: operator
-                }, 490);
-            });
-
-            // delay loading next page until animation is done
-            setTimeout(function(thing) {
-                loadContent(thing);
-            }, 500, {
-                name: self.attr("href"),
-                changeByName: false,
-
-                animate: {
-                    offset: -offset,
-                    operator: operator
-                }
-            });
+        // check if the tab that is clicked is already selected
+        if (newIndex == selectedIndex) {
+            return false;
         }
+
+        var offset = (newIndex > selectedIndex) ? 2000 : -2000;
+        var operator = "+=" + offset.toString();
+
+        // select the new tab
+        changeSelected(newIndex);
+
+        $(".animate").each(function() {
+            $(this).animate({
+                right: operator
+            }, 490);
+        });
+
+        // delay loading next page until animation is done
+        setTimeout(function(thing) {
+            loadContent(thing);
+        }, 500, {
+            name: self.attr("href"),
+            changeByName: false,
+
+            animate: {
+                offset: -offset,
+                operator: operator
+            }
+        });
+
         return false;
     });
 
+    // Arrow navigation
     $(document).keydown(function(event) {
         var keyCode = event.keyCode,
             offset,
@@ -164,21 +187,10 @@ $(document).ready(function() {
         });
 
         // delay loading next page until animation is done
-        setTimeout(function(thing) {
-            loadContent(thing);
+        setTimeout(function(loadArgs) {
+            loadContent(loadArgs);
         }, 500, data);
     });
-
-    var changeSelected = function(newIndex) {
-        if (selectedIndex != newIndex) {
-            if (selectedIndex != -1) {
-                $("ul#nav li:eq(" + selectedIndex.toString() + ")").toggleClass("active");
-            }
-            $("ul#nav li:eq(" + newIndex.toString() + ")").toggleClass("active");
-
-            selectedIndex = newIndex;
-        }
-    }
 
     $("ul#nav li").mouseover(function() {
         $(this).animate({
@@ -191,6 +203,4 @@ $(document).ready(function() {
             top: "+=4"
         }, 100);
     });
-
-
 });
